0fdf102b4219b28f809f3b9007b797d1c86337d2
Mauricio <carrilloimauricio15@gmail.com>
2020-12-31 15:14:35 -0600

    metodo get usando query params

--- express.js
+++ express.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',function(req,res){
-res.send('Hola soy express amigos!');
+app.get('/saludo',function(req,res){
+//res.send('Hola soy express amigos!');
+// Template strings
+res.send(`Hola ${req.query.name}`);
+
 });
 
 app.listen(4200);
