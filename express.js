47b7cf768f8c456d9d1f690514831143e19cd945
Mauricio <carrilloimauricio15@gmail.com>
2020-12-31 15:15:28 -0600

    metodo get usando query params

--- express.js
+++ express.js
@@ -5,8 +5,9 @@ const app = express();
 app.get('/saludo',function(req,res){
 //res.send('Hola soy express amigos!');
 // Template strings
+// backstrips permite incrustar codigo JS (Interpolacion)
 res.send(`Hola ${req.query.name}`);
-
+// peticion -> http://localhost:4200/saludo?name=mauricio
 });
 
 app.listen(4200);
