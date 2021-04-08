6deb6da29ecd735eee769fa574e177d719c5a729
mauricio <mauricio@pop-os.localdomain>
2021-04-08 02:06:35 -0500

    Add missing files

--- html/server.js
+++ html/server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
+
+//motor de vistas
+// primer parametro indica que usaremos un motor de vistas, segundo el nombre del motor
+//todas las vistas van dentro de la carpeta views
+app.set('view engine','ejs');
+
 //utilizando archivos estaticos a travez de la ruta /public con los archivos incluido en la carpeta assets
 // primer string -> Ruta de acceso a travez del navegador
 // segundo strin -> Nombre de la carpeta de archivos estaticos
-app.use('/public',express.static('assets'));
+app.use('/public',express.static('assets',{
+  // etag y maxAge es una propiedad que ayuda a la cache saber si el archivo fue modificado o no hanilitandolo o poniendo un tiempo para que sea esta la respueta
+  // en lugar de ir al servidor
+  etag: false,
+  maxAge: '5h'
+}));
+
 
 app.get('/',function(req,res){
-res.sendFile('index.html',{
+
+    res.render('index');
+//res.sendFile('index.html',{
     // variable que nos ayuda a definir la ruta absoluta de nuestro html 
     // aun cuando movamos el proyecto __dirname buscara la ruta donde se esta ejecutando este proyecto
-    root: __dirname
-});
+  //  root: __dirname
+//});
 });
 
 app.listen(3000);
